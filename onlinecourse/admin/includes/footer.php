abd21a02e838c5e62a2c4752b39f2ce3b02fcb13
Abiribah Ullash <54986400+abiribahullas7766@users.noreply.github.com>
2023-05-14 05:05:03 -0700

    Update footer.php

--- onlinecourse/admin/includes/footer.php
+++ onlinecourse/admin/includes/footer.php
@@ -2,7 +2,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
-                    &copy; <?php echo date('Y');?> Online Course Registration | By : <a href="http://www.phpgurukul.com/" target="_blank">PHPGURUKUL</a>
+                    &copy; <?php echo date('Y');?> Online Course Registration | By : <a href="https://md-abiribah-ullas.netlify.app/" target="_blank">Md Abiribah Ullas</a>
                 </div>
 
             </div>
